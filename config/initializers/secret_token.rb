1e5b131e4ca0b3d93da23e81328da30578a04ca0
Hristo Ganev <hganev@Hristos-Mac-mini.local>
2013-10-22 16:24:20 +0200

    new rails app with bootstrapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wanderr::Application.config.secret_token = '8a2ab8e2e817a2739b28044085d56cb4cc0cc7d466b121db1f3600f208e074594e56c9e62631d326713c2d5a2490bb51da2305598e81df3a5e057e014696703e'
